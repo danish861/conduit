0ed4f28c050acf50168d20ff28cea11c42af67a6
danish <danish.a@serverguy.com>
2023-01-14 12:48:51 +0530

    keyy"

--- components/Airticle/ArticleDetails.tsx
+++ components/Airticle/ArticleDetails.tsx
@@ -91,8 +91,10 @@ const ArticleDetails = ({
       <div className="mx-9  lg:mx-40 p-7 ">
         <p className="leading-7 text-lg mb-5">{body}</p>
         <ul className="flex">
-          {tagList?.map((tag) => (
-            <ArticlTag tag={tag} />
+          {tagList?.map((tag, index) => (
+            <div key={index}>
+              <ArticlTag tag={tag} />
+            </div>
           ))}
         </ul>
         <hr className="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700" />
